fbfe524edc66805e20729d8ccc5ed0ddba94f44c
Willis <137965272@qq.com>
2016-11-01 16:44:50 +0800

    modify format

--- md5/md5.h
+++ md5/md5.h
@@ -37,12 +37,5 @@ typedef struct
           a = ROTATE_LEFT(a,s); \
           a += b; \
           }
-void MD5Init(MD5_CTX *context);
-void MD5Update(MD5_CTX *context,unsigned char *input,unsigned int inputlen);
-void MD5Final(MD5_CTX *context,unsigned char digest[16]);
-void MD5Transform(unsigned int state[4],unsigned char block[64]);
-void MD5Encode(unsigned char *output,unsigned int *input,unsigned int len);
-void MD5Decode(unsigned int *output,unsigned char *input,unsigned int len);
 
 #endif
-
